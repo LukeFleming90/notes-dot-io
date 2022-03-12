e2d55cac2de9164afc408549c2091a294dd6eb3e
Luke Fleming <lukefleming@Lukes-MBP.fios-router.home>
2022-03-12 16:36:40 -0500

    Updated Index Page for Notes

--- views/notes/Index.jsx
+++ views/notes/Index.jsx
@@ -6,8 +6,29 @@ class Index extends React.Component {
         const { notes } = this.props;
         return (
             <DefaultLayout>
-                <a href="/user/logout"><button>Logout</button></a>
-                <hr/>
+                <nav className="navbar navbar-light bg-light p-3">
+                    <a className="navbar-brand d-flex flex-row" href="#">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="31"
+                            height="31"
+                            fill="currentColor"
+                            className="bi bi-journal pb-1 mx-2 mt-3"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+                            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+                        </svg>
+                        <p className="pt-1"style={{fontSize: "31px"}}>Notes.io</p>
+                    </a>
+                    <ul className="nav justify-content-end">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/user/logout">
+                                <button type="button" className="btn btn-outline-danger">Sign Out</button>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
                 <div className="d-flex justify-content-center pt-5">
                     <a href="/notes/new">
                         <button type="button" class="btn btn-warning">
